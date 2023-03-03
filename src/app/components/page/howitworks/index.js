c7055e474191d83847fe919afebd3c90ccc75684
darshit akbari <darshitakbari9999@gmail.com>
2023-03-03 23:26:11 +0530

    page -3

---
+++ src/app/components/page/howitworks/index.js
@@ -0,0 +1,44 @@
+const Howitworks = () => {
+    return (
+        <>
+            <div class="container mx-auto mb-40">
+                <div class="section--xs row center-xs">
+                    <h3 class="work cont2">How it Works</h3>
+                </div>
+                <div class="flex">
+                    <div class="grow work4">
+                        <div style={{ width: "100%;" }}>
+                            <h2 class="work1 mb-5">1</h2>
+                            <h5 class="work2 mb-5 mr-10">Get your Upgrade Card</h5>
+                        </div>
+                        <div class="work3">
+                            <p>Apply online and get a credit line from $500 to $25,000 with your Upgrade Card
+                                <sup>8</sup></p>
+                        </div>
+                    </div>
+                    <div class="grow work4">
+                        <div style={{ width: "100%;" }}>
+                            <h2 class="work1 mb-5">2</h2>
+                            <h5 class="work2 mb-5">Make purchases or get funds sent to your account</h5>
+                        </div>
+                        <div class="work3">
+                            <p>Pay with Upgrade Card at retail locations and online wherever Visa® is accepted or get funds sent to your bank account
+                                <sup>9</sup></p>
+                        </div>
+                    </div>
+                    <div class="grow work4">
+                        <div style={{ width: "100%;" }}>
+                            <h2 class="work1 mb-5">3</h2>
+                            <h5 class="work2 mb-5">Pay it down</h5>
+                        </div>
+                        <div class="work3">
+                            <p>Make monthly payments, which can be debited directly from your bank account
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Howitworks
